9659a86f3ba592be69138178a95dcd039003e1fd
Brendan Moy <bmoy@forwardfinancing.com>
2019-09-26 19:48:01 -0400

    add test

--- lib/astral_plane.rb
+++ lib/astral_plane.rb
@@ -1,5 +1,6 @@
 require "astral_plane/version"
 require 'astral_plane/http_client/abstract_http_client'
+require 'astral_plane/http_client/heroku'
 
 module AstralPlane
   class Error < StandardError; end
