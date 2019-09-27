a49c1527651e8fa07635ec0317cd7e5a28c21541
Brad Leyden <bleyden92@gmail.com>
2019-09-26 21:38:15 -0400

    Set up initial http request methods

--- lib/astral_plane/http_client/abstract_http_client.rb
+++ lib/astral_plane/http_client/abstract_http_client.rb
@@ -1,3 +1,6 @@
+require 'net/https'
+require 'uri'
+
 class AstralPlane::AbstractHttpClient
 
   def self.restart_server
@@ -14,5 +17,18 @@ class AstralPlane::AbstractHttpClient
 
   def self.list_servers
     raise NotImplementedError
+  end
+
+  def self.get(base_url, endpoint, token)
+    uri = URI.parse(base_url + endpoint)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "Bearer: #{token}"
+
+    [request, http]
+  end
+
+  def self.get_response(http, request)
+    http.request(request)
   end
 end
--- lib/astral_plane/http_client/heroku.rb
+++ lib/astral_plane/http_client/heroku.rb
@@ -1,7 +1,19 @@
 class AstralPlane::Heroku < AstralPlane::AbstractHttpClient
+  BASE_URL = 'https://heroku.com/api/v3/'.freeze
 
   def self.create_server
     :ok
   end
+
+
+  def self.get(base_url = BASE_URL, endpoint, token)
+    super_array = super
+    request = super_array[0]
+    http = super_array[1]
+    request['content-type'] = "application/json"
+
+    get_response(http, request)
+  end
+
 
 end
