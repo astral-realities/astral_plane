38e87204b546e045423a61dbd84c17222c2fb159
Brad Leyden <bleyden92@gmail.com>
2019-10-09 16:44:16 -0400

    use metaprogramming to dynamically create http methods

--- lib/astral_plane/http_client/abstract_http_client.rb
+++ lib/astral_plane/http_client/abstract_http_client.rb
@@ -3,6 +3,27 @@ require 'uri'
 
 class AstralPlane::AbstractHttpClient
 
+  METHODS = {
+    :post => "Post",
+    :get => "Get",
+    :delete => "Delete"
+  }.freeze
+
+  class << self
+    METHODS.each do |key, value|
+      define_method(key) do |base_url, endpoint, token, body|
+        uri = URI.parse(base_url + endpoint)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Object.const_get("Net::HTTP::#{value}").new(uri.request_uri)
+        request['Authorization'] = "Bearer: #{token}"
+        request['content-type'] = "application/json"
+        request.body = body if body
+
+        get_response(http, request)
+      end
+    end
+  end
+
   def self.restart_server
     raise NotImplementedError
   end
@@ -17,37 +38,6 @@ class AstralPlane::AbstractHttpClient
 
   def self.list_servers
     raise NotImplementedError
-  end
-
-  def self.get(base_url, endpoint, token)
-    uri = URI.parse(base_url + endpoint)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request['Authorization'] = "Bearer: #{token}"
-    request['content-type'] = "application/json"
-
-    get_response(http, request)
-  end
-
-  def self.post(base_url, endpoint, body, token)
-    uri = URI.parse(base_url + endpoint)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request['Authorization'] = "Bearer: #{token}"
-    request['content-type'] = "application/json"
-    request.body = body
-
-    get_response(http, request)
-  end
-
-  def self.delete(base_url, endpoint, token)
-    uri = URI.parse(base_url + endpoint)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Delete.new(uri.request_uri)
-    request['Authorization'] = "Bearer: #{token}"
-    request['content-type'] = "application/json"
-
-    get_response(http, request)
   end
 
   def self.get_response(http, request)
--- lib/astral_plane/http_client/digital_ocean.rb
+++ lib/astral_plane/http_client/digital_ocean.rb
@@ -1,22 +1,22 @@
 class AstralPlane::DigitalOcean < AstralPlane::AbstractHttpClient
   BASE_URL = 'https://api.digitalocean.com/v2/'.freeze
   # TODO: set this token in config
-  TOKEN = AstralPlane.digital_ocean_token
+  TOKEN = "token"
 
   def self.restart_server(droplet_id)
     body = '{"type":"reboot"}'
-    post(BASE_URL, "droplets/#{droplet_id}/actions", body, TOKEN)
+    post(BASE_URL, "droplets/#{droplet_id}/actions", TOKEN, body)
   end
 
   def self.create_server(body)
-    post(BASE_URL, "droplets", body, TOKEN)
+    post(BASE_URL, "droplets", TOKEN, body)
   end
 
   def self.delete_server(droplet_id)
-    delete(BASE_URL, "droplets/#{droplet_id}", TOKEN)
+    delete(BASE_URL, "droplets/#{droplet_id}", TOKEN, nil)
   end
 
   def self.list_servers
-    get(BASE_URL, "droplets", TOKEN)
+    get(BASE_URL, "droplets", TOKEN, nil)
   end
 end
