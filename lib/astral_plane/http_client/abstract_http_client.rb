617a4fe9ad11dce87d84bcb0105c34445fab53a2
Brendan Moy <bmoy@forwardfinancing.com>
2019-10-16 17:20:08 -0400

    list_servers returns 200

--- lib/astral_plane/http_client/abstract_http_client.rb
+++ lib/astral_plane/http_client/abstract_http_client.rb
@@ -9,22 +9,27 @@ class AstralPlane::AbstractHttpClient
     :get => "Get",
     :delete => "Delete"
   }.freeze
-# AstralPlane::Heroku.create_server("Ads", "asd")
+
+  # shared headers = all headers that are shared between vendors
+  # custom headers = headers that are unique for that vendors
+  # unique headers = set of unique headers on an individual request
+
   class << self
     METHODS.each do |key, value|
-      define_method(key) do |endpoint, token, headers = {}, body = {}|
+      define_method(key) do |endpoint, token, unique_headers = {}, body = {}|
         uri = URI.parse(base_url + endpoint)
         http = Net::HTTP.new(uri.host, uri.port)
         json_body = body.to_json
         http.use_ssl = true
         request = Object.const_get("Net::HTTP::#{value}").new(uri.request_uri)
 
-        self.combined_headers(token).merge(headers).each_pair do |header, value|
+        self.combined_headers(token).merge(unique_headers).each_pair do |header, value|
+          puts header
+          puts value
           request[header] = value
         end
-        # request['Content-Type'] = "application/json"
+        request['Content-Type'] = "application/json"
         request.body = json_body if body
-        binding.pry
         get_response(http, request)
       end
     end
@@ -54,9 +59,11 @@ class AstralPlane::AbstractHttpClient
     http.request(request)
   end
 
-  def self.headers(token = '')
+  # keep in mind this specific header is for OAuth api, if other vendors
+  # check for format
+  def self.shared_headers(token = '')
     {
-      'Authorization' => "Bearer: #{token}",
+      'Authorization' => "Bearer #{token}",
     }
   end
 
@@ -66,7 +73,7 @@ class AstralPlane::AbstractHttpClient
 
   def self.combined_headers(token)
     self
-      .headers(token)
+      .shared_headers(token)
       .merge(self.custom_headers)
   end
 end
--- lib/astral_plane/http_client/heroku.rb
+++ lib/astral_plane/http_client/heroku.rb
@@ -2,9 +2,14 @@ class AstralPlane::Heroku < AstralPlane::AbstractHttpClient
 
   # TODO: set this HEROKU_API_KEY in config
   HEROKU_API_KEY = "a45610ff-94b3-48cb-ac16-2e6527559f8c"
-  ACCEPT_HEADER = {
-        'Accept' => "application/vnd.heroku+json; version=3",
-  }
+  # ACCEPT_HEADER = {
+  #       'Accept' => "application/vnd.heroku+json; version=3",
+  # }
+
+  def self.custom_headers()
+    {  'Accept' => "application/vnd.heroku+json; version=3",}
+  end
+
 
   def self.base_url
     'https://api.heroku.com'.freeze
@@ -12,22 +17,22 @@ class AstralPlane::Heroku < AstralPlane::AbstractHttpClient
 
   # Can be app id or app name
   def self.create_server(app_name, body)
-    post("/apps/#{app_name}/dynos", HEROKU_API_KEY, ACCEPT_HEADER, body)
+    post("/apps/#{app_name}/dynos", HEROKU_API_KEY, body)
   end
 
   # heroku uses delete request to restart a dyno
   def self.restart_server(app_name, dyno_name)
-    delete("/apps/#{app_name}/dynos/#{dyno_name}", HEROKU_API_KEY, ACCEPT_HEADER, nil)
+    delete("/apps/#{app_name}/dynos/#{dyno_name}", HEROKU_API_KEY, nil)
   end
 
   #heroku uses post request to delete dyno
   def self.delete_server(app_name, dyno_name)
-    post("/apps/#{app_name}/dynos/#{dyno_name}", HEROKU_API_KEY, ACCEPT_HEADER, nil)
+    post("/apps/#{app_name}/dynos/#{dyno_name}", HEROKU_API_KEY, nil)
   end
 
   # does not require header: content type json
-  def self.list_server(app_name)
-    get("/apps/#{app_name}/dynos", HEROKU_API_KEY, ACCEPT_HEADER)
+  def self.list_servers(app_name)
+    get("/apps/#{app_name}/dynos", HEROKU_API_KEY)
   end
 
 
